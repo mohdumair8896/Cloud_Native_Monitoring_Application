7e39b1d8b9bb3acba511d4858dbb38d3d0719191
mohdumair8896 <sajidali27726@gmail.com>
2024-06-27 03:18:43 +0530

    ECR to create container on AWS

---
+++ ecr.py
@@ -0,0 +1,11 @@
+from collections.abc import Mapping
+from collections.abc import MutableMapping
+import boto3
+
+ecr_client = boto3.client('ecr')
+
+repository_name = "my_monitoring_app_image"
+response = ecr_client.create_repository(repositoryName=repository_name)
+
+repository_uri = response ['repository']['repositoryUri']
+print(repository_uri)
